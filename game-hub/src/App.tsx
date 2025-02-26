b9461f3257daa8d7c105e13bf2f2ab6b8e4f713e
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-26 16:01:37 +0530

    Building A Responsive Layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,10 +1,34 @@
-import { Button, HStack } from "@chakra-ui/react";
+import {
+  Button,
+  Grid,
+  GridItem,
+  HStack,
+  Show,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 function App() {
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg={"pink"}>
+        Nav
+      </GridItem>
+      {isLargeScreen && (
+        <Show when={isLargeScreen}>
+          <GridItem area="aside" bg={"gray"}>
+            Aside
+          </GridItem>
+        </Show>
+      )}
+      <GridItem area="main" bg={"salmon"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
