454fa25dac0bd85c89aa89a47cc7fbf0a9eb0030
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-28 11:55:27 +0530

    Displaying Critic Score

--- game-hub/src/Comp/GameCard.tsx
+++ game-hub/src/Comp/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Game } from "@/Hooks/useGames";
-import { Card, Image, Text } from "@chakra-ui/react";
+import { Card, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -9,15 +10,20 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={"5px"} maxW={"sm"} overflow={"hidden"}>
-      <Image src={game.background_image} bgSize={40} />
-      <Card.Body>
-        <Card.Title fontSize={"2xl"}>{game.name}</Card.Title>
-        <PlatformIconList
-          platfroms={game.parent_platforms.map((p) => p.platform)}
-        />
-      </Card.Body>
-    </Card.Root>
+    <>
+      <Card.Root borderRadius={"5px"} maxW={"sm"} overflow={"hidden"}>
+        <Image src={game.background_image} bgSize={40} />
+        <Card.Body>
+          <Card.Title fontSize={"2xl"}>{game.name}</Card.Title>
+          <HStack justifyContent={"space-between"}>
+            <PlatformIconList
+              platfroms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </Card.Body>
+      </Card.Root>
+    </>
   );
 };
 
